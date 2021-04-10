c212192a1f58e774039989dd2c9e78cf9d400cd3
Mitzi17 <m_estrada17@live.com>
2021-04-09 17:07:56 -0700

    commit tree 62ff04563187370ada9e4f4fb0b9e7442c269270

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+// a const variable that is assigned an anonymous function
+const returnFirstTwoDrivers = function(array) {
+    return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(array) {
+    return array.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// higher-order function that takes in one argument, and returns a function
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+        return fare * integer
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(driversArr, returnDrivers) {
+    return returnDrivers(driversArr)
+} 
